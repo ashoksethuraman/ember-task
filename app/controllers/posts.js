1aa35080951629de2196423f05d441f3afd0803c
ashokkumar <ashok.s@bambeeq.com>
2017-09-07 15:44:56 +0530

    clean button

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -5,13 +5,43 @@ export default Ember.Controller.extend({
 
  actions: {
     expand: function(post) {
-      this.set('isExpanded', true);
-      // console.log(post.get('title'))
+      isConfirmationModel : true;
+      console.log('conformation models::',isConfirmationModel);
+
+      // ... Then render the modal
+      // modal.show();
+
+      // var message = null;
+      //     var xhr = $.ajax({
+      //             url: 'http://www.splashbase.co/api/v1/images/search?query=bird',
+      //             dataType: 'json',
+      //             contentType: 'application/json; charset=utf-8',
+      //             type: 'GET',
+      //             cors: true ,
+      //             headers: {
+      //                  'Access-Control-Allow-Origin': '*',
+      //              },
+      //             async: false,
+      //             success: function (data) {
+      //                 message = data;
+      //                 console.log('outputes:::::');
+      //                 console.log(message);
+      //             }
+      //         });
+      //
+      //     if (xhr.status != 200) { // error
+      //         message = { errorCode: xhr.status, errorMessage: xhr.statusText };
+      //     }
+      //
+      //     return message;
+
+
     },
 
     close: function() {
     	 this.set('isShowingModal', false)
     }
   }
+
 
 });
